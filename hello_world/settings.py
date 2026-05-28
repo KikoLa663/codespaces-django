985ae3009178dc33a20bb1d53167083fc615e561
KikoLa663 <152925504+KikoLa663@users.noreply.github.com>
2026-05-28 20:13:27 +0300

    Update settings.py

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/topics/settings/
 """
-
+import dj_database_url 
 import os
 from pathlib import Path
 from decouple import config
@@ -85,8 +85,13 @@ WSGI_APPLICATION = "hello_world.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+     "ENGINE": "django.db.backends.sqlite3",
+      "NAME": BASE_DIR / "db.sqlite3",
+       # default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+       # default='postgresql://postgres:postgres@localhost:5432/mysite',
+        #conn_max_age=600
+    #)
     }
 }
 
